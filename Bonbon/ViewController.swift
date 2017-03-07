0e25565ad77b2c2b98b5b61bc6d586d2cc919cbf
embydextrous <arjit.agarwal.1000@gmail.com>
2017-03-07 18:27:28 +0530

    Added a button and code to change color on button tap.

--- Bonbon/ViewController.swift
+++ Bonbon/ViewController.swift
@@ -20,6 +20,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changeColor(sender: UIButton) {
+        let r = CGFloat(arc4random() % 255)
+        let g = CGFloat(arc4random() % 255)
+        let b = CGFloat(arc4random() % 255)
+        
+        // Create Color
+        let color = UIColor(red: (r/255.0), green: (g/255.0), blue: (b/255.0), alpha: 1.0)
+        
+        // Update View
+        view.backgroundColor = color
+    }
 
 }
 
